cfefa3849a2597f6777026cb2bca6699a50c61d5
Rigo <rigo.reatiga@gmail.com>
2018-03-10 18:53:17 -0600

    User login tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
     validates :uid, presence: true
     validates :provider, presence: true
     has_many :tokens
+
+    def self.from_omniauth(data)
+			#Data hash
+			#{provider: 'facebook', uid:'12345', info:{email:'...', name:'Rigo'}}
+			User.where(provider: data[:provider], uid: data[:uid]).first_or_create do |user|
+				user.email = data[:info][:email]
+			end
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe User, type: :model do
   it { should allow_value("rigo@email.com").for(:email) }
   it { should validate_presence_of(:uid) }
   it { should validate_presence_of(:provider) }
+
+  it "should create a new user if uid and provider does not exists" do
+    expect{
+      User.from_omniauth({uid: "12345", provider: "facebook", info:{email:"rigo@mail.com"}}) 
+    }.to change(User, :count).by(1)
+  end
+
+  it "should find an user if uid and provider already exists" do
+    user = FactoryBot.create(:user)
+    expect{
+      User.from_omniauth({uid: user.uid, provider: user.provider, info:{email:"rigo@mail.com"}}) 
+    }.to change(User, :count).by(0)
+  end
+
+  it "should return found user if uid and provider already exists" do
+    user = FactoryBot.create(:user)
+    expect(
+      User.from_omniauth({uid: user.uid, provider: user.provider}) 
+    ).to eq(user)
+  end
 end
