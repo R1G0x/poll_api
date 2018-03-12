46c25fd0ae6aae06f180ac8f02e61bd45e41b655
Rigo <rigo.reatiga@gmail.com>
2018-03-11 23:45:00 -0600

    Authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
-    
+    before_action :authenticate
+    def authenticate
+        token_str = params[:token]
+        token = Token.find_by(token: token_str)
+
+        if token.nil? || !token.is_valid?
+            render json: { error: "Your token is not valid", status: :unauthorized}
+        else
+            @current_user = token.user
+        end
+    end
 end
