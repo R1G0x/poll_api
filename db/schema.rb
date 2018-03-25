a1dc8f64836dc5f8498af110c5c92cba00dda9c8
Rigo <rigo.reatiga@gmail.com>
2018-03-25 10:50:16 -0600

    Create polls and its validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310234914) do
+ActiveRecord::Schema.define(version: 20180325152643) do
+
+  create_table "my_polls", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "expires_at"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_my_polls_on_user_id"
+  end
 
   create_table "tokens", force: :cascade do |t|
     t.datetime "expires_at"
---
+++ spec/factories/my_polls.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :my_poll do
+    user nil
+    expires_at "2018-03-25 09:26:43"
+    title "MyString"
+    description "MyText"
+  end
+end
