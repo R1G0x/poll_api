5311e275af4049acaf770f4f8bd519484bf500db
Rigo <rigo.reatiga@gmail.com>
2018-03-25 23:03:30 -0600

    Delete polls

--- app/controllers/api/v1/my_polls_controller.rb
+++ app/controllers/api/v1/my_polls_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MyPollsController < ApplicationController
     before_action :authenticate, only:[:create, :update, :destroy]
+    before_action :set_poll, only:[:show,:update,:destroy]
     def index
         @polls = MyPoll.all
     end 
 
     def show 
-        @poll = MyPoll.find(params[:id])
     end 
 
     def create
@@ -26,17 +26,32 @@ class Api::V1::MyPollsController < ApplicationController
             render :show
         else 
             render json:{
-                errors: "You don't have permissions to modify this poll",
+                errors: "You don't have permissions to update this poll",
                 status: :unauthorized
             }
         end
     end
 
     def destroy
+        if @poll.user == @current_user
+            @poll.destroy
+            render json: {
+                message: "Poll deleted"
+            }
+        else 
+            render json:{
+                errors: "You don't have permissions to delete this poll",
+                status: :unauthorized
+            }
+        end
     end 
 
     private 
     def my_polls_params
         params.require(:poll).permit(:title, :description, :expires_at)
+    end
+
+    def set_poll
+        @poll = MyPoll.find(params[:id])
     end
 end
