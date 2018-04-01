54de3ff1733c7fe7445e9e32cf9ff66202d7bba3
Rigo <rigo.reatiga@gmail.com>
2018-04-01 00:36:42 -0600

    Create questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,14 +1,24 @@
 class Api::V1::QuestionsController < ApplicationController
     before_action :authenticate, except: [:index, :show]
     before_action :set_question, only:[:update, :show, :delete]
-
+    before_action :set_poll
     def index
+        @questions = @poll.questions
     end
 
     def show 
     end 
 
     def create 
+        @question = @poll.questions.new(question_params)
+        if @question.save 
+            render template: "api/v1/questions/show"
+        else
+            render json: {
+                error: @question.errors.full_messages,
+                status: :unprocessable_entity
+            }
+        end 
     end 
      
     def update 
@@ -24,5 +34,9 @@ class Api::V1::QuestionsController < ApplicationController
 
     def set_question
         @question = Question.find(params[:id])
-    end 
+    end
+    
+    def set_poll 
+        @poll = MyPoll.find(params[:poll_id])
+    end
 end
